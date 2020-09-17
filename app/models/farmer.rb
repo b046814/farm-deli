0dec1c9bb4a6035c3d0ecea5645674146e39cf5f
b046814 <ub046814@gmail.com>
2020-09-17 21:46:10 +0900

    userモデル、farmerモデルのバリデーション設定

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -5,7 +5,12 @@ class Farmer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 attr_accessor :organic
-validates_acceptance_of :organic, allow_nil: false, on: :create
-
+validates_acceptance_of :organic, allow_nil: false, on: :create, message: '有機農産物のみの販売に同意が必要です'
+validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: { message:'入力が必要です'}
+validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:'半角数字と英字の両方を入力して下さい' }
+validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:'全角ひらがな、カタカナ、漢字を入力して下さい' }
+validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:'全角ひらがな、カタカナ、漢字を入力して下さい' }
+validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message:'全角カタカナを入力して下さい' }
+validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message:'全角カタカナを入力して下さい' }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: { message: '入力が必要です' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英字と数字を両方含めてください' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角ひらがな、カタカナ、漢字で入力して下さい' }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角ひらがな、カタカナ、漢字で入力して下さい' }
+  validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナで入力して下さい' }
+  validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナで入力して下さい' }
+
 end
