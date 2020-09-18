95f42ea974f159c375be70db80e7bf4e7f4e7c8d
b046814 <ub046814@gmail.com>
2020-09-18 12:33:41 +0900

    farmsコントローラーのアクション設定

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,2 +1,19 @@
 class FarmsController < ApplicationController
+  def new
+    @farm = Farm.new 
+  end
+
+  def create
+    @farm = Farm.new(farms_params)
+    if @farm.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def farms_params
+    params.require(:farm).permit(:name, :prefecture_id, :description, :feature, image: []).merge(farmer_id: current_farmer.id)
 end
