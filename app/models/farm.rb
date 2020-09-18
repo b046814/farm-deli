13ebbe79a19642308a41044112f436f46d44c2f8
b046814 <ub046814@gmail.com>
2020-09-18 12:02:40 +0900

    ActiveHashの導入

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,4 +1,8 @@
 class Farm < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :farmer
+
+  validates :name, :description, :feature, :prefecture_id, presence: true
+  validates :prefecture_id, numericarity: { other_than: 1 }
 end
