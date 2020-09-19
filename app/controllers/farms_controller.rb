7dba35ccb03e70ed010b6add928c35fa9e7f508a
b046814 <ub046814@gmail.com>
2020-09-19 09:22:50 +0900

    farm紹介編集ページ実装

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -21,15 +21,18 @@ class FarmsController < ApplicationController
   end
 
   def update
-    farm = Farm.find(params[:id])
+    @farm = Farm.find(params[:id])
     if params[:farm][:image_ids]
       params[:farm][:image_ids].each do |image_id|
-        image = post.images.find(image_id)
+        image = @farm.images.find(image_id)
         image.purge
       end
     end
-    if farm.update(farms_params)
-      redirect_to farm_path(farm.id)
+    if farms_params[:images]
+      @farm.images.attach(farms_params[:images])
+    end
+    if @farm.update(name: farms_params[:name], prefecture_id: farms_params[:prefecture_id], description: farms_params[:description], feature: farms_params[:feature], farmer_id: farms_params[:farmer_id])
+        redirect_to farm_path(@farm.id)
     else
       render :edit
     end
--- app/models/farm.rb
+++ app/models/farm.rb
@@ -3,7 +3,8 @@ class Farm < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :farmer
   has_many_attached :images
-
+  
+  validates :farmer_id, uniqueness: true
   validates :name, :images, :description, :feature, :prefecture_id, presence: true
   validates :prefecture_id, numericality: { other_than: 1 }
 end
