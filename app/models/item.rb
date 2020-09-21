d7d290c0a9ff6f7df2233750a8e92300d876cc6e
b046814 <ub046814@gmail.com>
2020-09-21 14:06:17 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :farmer
+
+  validates :name, :description, :ship_expense_id, :prefecture_id, :period_id, :price, presence: true
+  validates :ship_expense_id, :prefecture_id, :period_id, numericality: { other_than: 1 }
+  validates :name, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナで入力して下さい' }
+  
 end
