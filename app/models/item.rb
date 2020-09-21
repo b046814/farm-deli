77105b7da30beeabccf1b4f47b71ce34ee7a8628
b046814 <ub046814@gmail.com>
2020-09-21 15:06:51 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :farmer
+  has_one_attached :image
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :period
+  belongs_to_active_hash :ship_expense
 
-  validates :name, :description, :ship_expense_id, :prefecture_id, :period_id, :price, presence: true
+  validates :name, :image, :description, :ship_expense_id, :prefecture_id, :period_id, :price, presence: true
   validates :ship_expense_id, :prefecture_id, :period_id, numericality: { other_than: 1 }
   validates :name, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナで入力して下さい' }
-  
+
 end
