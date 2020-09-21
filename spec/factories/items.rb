8564153e202cb1e0fd14ccfe6a932b6f3cc30ccb
b046814 <ub046814@gmail.com>
2020-09-21 17:58:35 +0900

    itemモデルの単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
+    name { 'ダイコン' }
+    description { Faker::Lorem.sentence }
+    ship_expense_id { '2' }
+    prefecture_id { '37' }
+    period_id { '3' }
+    price { '9999' }
+    association :farmer
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png' )
+    end
   end
 end
