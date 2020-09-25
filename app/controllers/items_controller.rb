aae6aa91d94cfd696542e0311b19a31da3fff023
b046814 <ub046814@gmail.com>
2020-09-25 20:33:05 +0900

    商品編集用コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_farmer!, only: :new
+  before_action :set_item, only: [:show, :edit, :update]
+
 
   def index
     @farms = Farm.includes(:farmer)
@@ -25,13 +27,27 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if item.save
+      redirect_to item_path(item)
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :ship_expense_id, :prefecture_id, :period_id, :price, :image).merge(farmer_id: current_farmer.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
