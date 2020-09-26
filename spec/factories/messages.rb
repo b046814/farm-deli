622231b63ddfe3d4a881a0492d6b6a9df70e47cf
b046814 <ub046814@gmail.com>
2020-09-26 16:55:47 +0900

    messageモデルの単体テストコード

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :message do
-    
+    content { Faker::Lorem.sentence }
+    id_user { true }
+
+    association :room
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    
+    before do
+      @message = FactoryBot.build(:message)
+    end
+
+    context 'メッセージを投稿できる時' do
+      it '全ての情報が入力されていれば投稿できる' do
+        expect(@message).to be_valid
+      end
+    end
+
+    context 'メッセージを投稿できない時' do
+      it 'contentが空では投稿できない' do
+        @message.content = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'id_userが空では投稿できない' do
+        @message.id_user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Id user can't be blank")
+      end
+
+      it '紐づくroomが無ければ投稿できない' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Room must exist")
+      end
+    end
+  end
 end
