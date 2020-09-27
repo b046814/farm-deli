f3c0aee2cec8227c7b09fd33a795e1624453053d
b046814 <ub046814@gmail.com>
2020-09-27 17:18:08 +0900

    商品購入のためのビューファイル、コントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,14 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    @order_address.item_id = @item.id 
     @order_address.item_count = params[:count]
     @order_address.amount = (@item.price * params[:count].to_i)
   end
 
   def create
-    binding.pry
-    @order_address = OrderAdress.new(order_params)
+    @order_address = OrderAddress.new(order_params)
+    @order_address.token = params[:token]
     if @order_address.save
       redirect_to root_path
     else
@@ -20,7 +21,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:token, :item_id, order_address: [:item_count, :postal_code, :prefecture_id, :city, :block_number, :building, :phone_number]).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:item_id, :item_count, :postal_code, :prefecture_id, :city, :block_number, :building, :phone_number).merge(user_id: current_user.id)
   end
 
 end
