b1808d626307a9dc1d30b1e1938b0be63219d603
b046814 <ub046814@gmail.com>
2020-09-27 09:33:51 +0900

    購入機能のためのコントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+
+  def new
+    @item = Item.find(params[:id])
+    @order_address = OrderAdress.new
+  end
+
+  def create
+    @order_address = OrderAdress.new(order_params)
+    if @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:token, :item_id, order_address: [:item_count, :postal_code, :prefecture_id, :city, :block_number, :building, :phone_number]).merge(user_id: current_user.id)
+  end
+
 end
