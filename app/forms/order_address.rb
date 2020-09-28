265dc0656f115fa4f452b328e44981b8ea5b20a0
b046814 <ub046814@gmail.com>
2020-09-28 14:11:42 +0900

    フォームオブジェクトOrderAddress編集

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,18 +1,18 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :item_count, :token, :amount, \
-                :postal_code, :prefecture_id, :city_name, :block_number, :building, :phone_number
+                :postal_code, :prefecture_id, :city, :block_number, :building, :phone_number
 
   POSTAL_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/.freeze
   PHONE_NUMBER_REGEX = /\A[0-9]{1,11}\z/.freeze
-  validates :token, :item_count, :postal_code, :prefecture_id, :city_name, :block_number, :phone_number, presence: true
+  validates :token, :user_id, :item_count, :amount, :item_id, :postal_code, :prefecture_id, :city, :block_number, :phone_number, presence: true
   validates :postal_code, format: { with: POSTAL_CODE_REGEX }
   validates :phone_number, format: { with: PHONE_NUMBER_REGEX }
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id, item_count: item_count)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city_name: city_name, \
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, \
                    block_number: block_number, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
