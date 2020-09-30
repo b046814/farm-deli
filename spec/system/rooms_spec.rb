86d59a28e7fb221e9d64bb13310b146320771fd3
b046814 <ub046814@gmail.com>
2020-09-30 15:13:00 +0900

    いいね機能結合テストコード

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -7,12 +7,17 @@ RSpec.describe "チャットルームの削除機能", type: :system do
   end
 
   it 'チャットルームを削除すると、関連するメッセージが全て削除される' do
+    # 消費者でログイン
     sign_in_user(@room.user)
+    # チャットルームへ移動
     visit room_path(@room)
+    # メッセージを5個作成
     FactoryBot.create_list(:message, 5, room_id: @room.id)
+    # チャットを削除するボタンを押すとメッセージが5個減っていることを確認
     expect{
       find_link("チャットを削除する", href: room_path(@room)).click
     }.to change { @room.messages.count }.by(-5)
+    # トップページに移動していることを確認
     expect(current_path).to eq root_path
   end
 end
