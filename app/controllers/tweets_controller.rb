129a71c5052d5b4ebb867cd748b7bb8762b6f29e
b046814 <ub046814@gmail.com>
2020-10-01 19:12:53 +0900

    ツイート一覧表示ビューファイル設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,16 @@
 class TweetsController < ApplicationController
+  before_action :set_farm, only: [:index, :new, :create, :edit, :update, :destroy]
+  before_action :move_to_root, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @tweets = @farm.tweets
+  end
 
   def new
-    @farm = Farm.find(params[:farm_id])
     @tweet = Tweet.new
   end
 
   def create
-    @farm = Farm.find(params[:farm_id])
     @tweet = Tweet.new(tweet_params)
     @tweet.farm_id = @farm.id
     if @tweet.save
@@ -16,10 +20,43 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to farm_tweets_path(@farm)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+      redirect_to farm_tweets_path(@farm)
+    else
+      redirect_to farm_tweets_path(@farm)
+    end
+  end
+
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:content, :image)
+  end
+
+  def set_farm
+    @farm = Farm.find(params[:farm_id])
+  end
+
+  def move_to_root
+    return if farmer_signed_in? && current_farmer.farm.id == @farm.id
+
+    redirect_to root_path
   end
 
 end
