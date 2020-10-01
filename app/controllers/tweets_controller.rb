41131b62c0080b02ddc7e9c04c6e5b8cd9c631ed
b046814 <ub046814@gmail.com>
2020-10-01 15:09:28 +0900

    ツイート機能new,createアクションと対応するビューの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,25 @@
 class TweetsController < ApplicationController
+
+  def new
+    @farm = Farm.find(params[:farm_id])
+    @tweet = Tweet.new
+  end
+
+  def create
+    @farm = Farm.find(params[:farm_id])
+    @tweet = Tweet.new(tweet_params)
+    @tweet.farm_id = @farm.id
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :image)
+  end
+
 end
